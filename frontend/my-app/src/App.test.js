34399b469648ae92841d89178201b4eb1f9961ff
Alif-2001 <62363450+Alif-2001@users.noreply.github.com>
2023-02-14 12:02:59 -0500

    Added more tests (#2)

--- frontend/my-app/src/App.test.js
+++ frontend/my-app/src/App.test.js
@@ -1,9 +1,61 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
+import SentimentViewer from './Components/SentimentViewer';
 
-test('renders learn react link', () => {
+test('renders Home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Sentiment Analysis/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Sentiment Analysis/i);
+  const textBox = screen.getByLabelText(/Your Text:/i);
+  const submitButton = screen.getByRole('button');
+  expect(title).toBeInTheDocument();
+  expect(textBox).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton).toBeEnabled();
+  expect(screen.queryByRole('sentiment-viewer')).not.toBeInTheDocument();
+});
+
+test('Sentiment Viewer', () => {
+  render(<SentimentViewer data={{
+      score: 0,
+      comparative: 1.3,
+      positive: ["nice", "pretty"],
+      negative: ["wrong", "not"]
+    }
+  }/>)
+
+  expect(screen.getByText(/Score/i)).toBeInTheDocument();
+  expect(screen.getByRole('score')).toHaveTextContent(0);
+  expect(screen.getByText(/Comparative/i)).toBeInTheDocument();
+  expect(screen.getByRole('comparative')).toHaveTextContent(1.3);
+  expect(screen.getByText(/Positive Words/i)).toBeInTheDocument();
+  expect(screen.getByRole('positive-words')).toMatchInlineSnapshot(`
+      <div
+        role="positive-words"
+      >
+        <ul>
+          <li>
+            nice
+          </li>
+          <li>
+            pretty
+          </li>
+        </ul>
+      </div>
+  `)
+  expect(screen.getByText(/Negative Words/i)).toBeInTheDocument();
+  expect(screen.getByRole('negative-words')).toMatchInlineSnapshot(`
+    <div
+      role="negative-words"
+    >
+      <ul>
+        <li>
+          wrong
+        </li>
+        <li>
+          not
+        </li>
+      </ul>
+    </div>
+  `)
 });
--- frontend/my-app/src/Components/SentimentViewer.js
+++ frontend/my-app/src/Components/SentimentViewer.js
@@ -4,7 +4,7 @@ export default function SentimentViewer(props) {
     if (!props) {
         return (<div></div>)
     }
-
+    console.log(props);
     const generateList = (items) => {
         const rows = []
         items.forEach(element => {
@@ -18,25 +18,33 @@ export default function SentimentViewer(props) {
     return (
         <div>
             <h3>Score</h3>
-            {props.data.score}
+            <div role="score">
+                {props.data.score}
+            </div>
             <h3>Comparative</h3>
-            {props.data.comparative}
+            <div role="comparative">
+                {props.data.comparative}
+            </div>
             <h3>Positive Words</h3>
-            {props.data.positive.length > 0 ?
-                <ul>
-                    {generateList(props.data.positive)}
-                </ul>
-                :
-                <h5>No positive words</h5>
-            }
+            <div role="positive-words">
+                {props.data.positive.length > 0 ?
+                    <ul>
+                        {generateList(props.data.positive)}
+                    </ul>
+                    :
+                    <h5>No positive words</h5>
+                }
+            </div>
             <h3>Negative Words</h3>
-            {props.data.negative.length > 0 ?
-                <ul>
-                    {generateList(props.data.negative)}
-                </ul>
-                :
-                <h5>No negative words</h5>
-            }
+            <div role="negative-words">
+                {props.data.negative.length > 0 ?
+                    <ul>
+                        {generateList(props.data.negative)}
+                    </ul>
+                    :
+                    <h5>No negative words</h5>
+                }
+            </div>
         </div>
     )
 }
